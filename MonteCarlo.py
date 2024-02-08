9d714bf1c30b0d452e5bcdb8cf9d33ed7c5ca814
Julia_Tsai <juliamtw20@gmail.com>
2024-02-08 14:22:32 -0500

    update

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -3,14 +3,20 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from datetime import datetime, timedelta
 
 
 tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'META', 'NFLX', 'VTI', 'SPY', 'JNJ', 'KO', 'XLK', 'XLF']
 Close = pd.DataFrame()
-start_date = '2021-01-01'
+start_date = datetime(2010,1,1).date()
+end_date = datetime.now().date()
+start_date_rf = end_date - timedelta(days=365)
+
+IRX = yf.download('^IRX', start= start_date_rf, end = end_date)
+rf = IRX['Close'].pct_change().dropna().mean()
 
 for ticker in tickers:
-    Close[ticker] = yf.download(ticker, start= start_date, end='2024-12-31')['Close']
+    Close[ticker] = yf.download(ticker, start= start_date, end= end_date)['Close']
 
 print(Close.describe())
 
@@ -24,8 +30,8 @@ plt.show()
 
 
 Return = Close.pct_change()
-print(Return.describe())
 Return = Return.dropna()
+print(Return.describe())
 
 plt.plot(Return.index, Return)
 plt.title('Stock Daily Return')
@@ -46,9 +52,9 @@ plt.show()
 
 
 T = (Return.index.max() - Return.index.min()).days
-n_simulation = 20000
+n_simulation = 2000
 
-simulation_output = {'weights': [], 'perform': {'return': [], 'std': []}}
+simulation_output = {'weights': [], 'sharpe': [], 'perform': {'return': [], 'std': []}}
 
 for i in range(0, n_simulation):
 
@@ -56,21 +62,28 @@ for i in range(0, n_simulation):
     weights /= np.sum(weights)
 
     wtRet = (Return * weights).sum(axis=1)
+    
+    sharpe = (wtRet.mean() - rf) / wtRet.std()
 
     simulation_output['weights'].append(weights)
+    simulation_output['sharpe'].append(sharpe)
     simulation_output['perform']['return'].append(wtRet.mean())
     simulation_output['perform']['std'].append(wtRet.std())
 
 simulation_output['weights'] = pd.DataFrame(simulation_output['weights'])
-simulation_output['weights'].columns = tickers
-perform = pd.concat([simulation_output['weights'],
-                     pd.DataFrame(simulation_output['perform'])], axis = 1)
+perform = pd.DataFrame(simulation_output['perform'])
 
 
 plt.title('Simulations')
-plt.xlabel('Return')
-plt.ylabel('STD')
-plt.scatter(perform['std'], perform['return'])
+plt.xlabel('STD')
+plt.ylabel('Return')
+plt.scatter(x = perform['std'], y = perform['return'])
 plt.show()
 
+maxSharpe_index = np.argmax(simulation_output['sharpe'])
+maxSharpe_weights = simulation_output['weights'].iloc[maxSharpe_index]
+maxSharpe_weights = pd.DataFrame({'ticker': tickers, 'weight': maxSharpe_weights})
+maxSharpe_perform = perform.iloc[maxSharpe_index]
 
+
+print([maxSharpe_weights, maxSharpe_perform])
--- MonteCarlo_stream.py
+++ MonteCarlo_stream.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import streamlit as st
+from datetime import datetime, timedelta
 
 st.title("Monte Carlo on Stocks")
 
@@ -12,27 +13,37 @@ tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'META', 'NFLX', 'VTI', 'SPY', 'JNJ',
 
 selected_tickers = st.multiselect("Tickers:",tickers)
 
-start_date = '2021-01-01'
+start_date = datetime(2010,1,1).date()
+end_date = datetime.now().date()
+start_date_rf = end_date - timedelta(days=365)
+
+IRX = yf.download('^IRX', start= start_date_rf, end = end_date)
+rf = IRX['Close'].pct_change().dropna().mean()
+
 
 Close = pd.DataFrame()
 for ticker in selected_tickers:
-    Close[ticker] = yf.download(ticker, start= start_date, end='2024-12-31')['Close']
-
-#st!
-st.write('Price')
-st.line_chart(Close)
+    Close[ticker] = yf.download(ticker, start= start_date, end= end_date)['Close']
 
 Return = Close.pct_change()
 print(Return.describe())
 Return = Return.dropna()
 
+mean_sd_return = pd.DataFrame({'Daily Return': Return.mean(), 'std': Return.std()}).T
+mat_cor = Return.corr()
+
+#st!
+st.write('Price')
+st.line_chart(Close)
+
 # st!
 st.write('Returns')
 st.line_chart(Return)
 
+# st!
+st.write('Performance: mean return and standard deviation')
+st.table(mean_sd_return)
 
-mean_sd_return = pd.DataFrame({'mean': Return.mean(), 'std': Return.std()}).T
-mat_cor = Return.corr()
 
 plt.figure(figsize= (5, 3))
 sns.heatmap(mat_cor, annot = True, cmap = 'coolwarm', fmt=".2f", annot_kws={"size": 6})
@@ -55,7 +66,7 @@ st.header('Select number of Simulation')
 n_simulation = st.slider('Number of Simulation', 1000, 20000)
 button2 = st.button('Start Stimulating!')
 
-simulation_output = {'weights': [], 'perform': {'return': [], 'std': []}}
+simulation_output = {'weights': [], 'sharpe': [], 'perform': {'return': [], 'std': []}}
 
 if button2:
     for i in range(0, n_simulation):
@@ -64,8 +75,11 @@ if button2:
         weights /= np.sum(weights)
 
         wtRet = (Return * weights).sum(axis=1)
+        
+        sharpe = (wtRet.mean() - rf) / wtRet.std()
 
         simulation_output['weights'].append(weights)
+        simulation_output['sharpe'].append(sharpe)
         simulation_output['perform']['return'].append(wtRet.mean())
         simulation_output['perform']['std'].append(wtRet.std())
 
@@ -73,7 +87,21 @@ if button2:
     perform = pd.DataFrame(simulation_output['perform'])
     st.write('Stimulation Scatter Plot')
     st.scatter_chart(perform, x = 'std', y = 'return')
-
+    
+    maxSharpe_index = np.argmax(simulation_output['sharpe'])
+    maxSharpe_weights = simulation_output['weights'].iloc[maxSharpe_index]
+    maxSharpe_weights = pd.DataFrame({'ticker': selected_tickers, 'weight': maxSharpe_weights})
+    maxSharpe_perform = pd.DataFrame(perform.iloc[maxSharpe_index])
+    
+    st.write('Sharpe Ratios')
+    st.bar_chart(simulation_output['sharpe'])
+    st.write('Highest Sharpe Ratio:' + str(round(max(simulation_output['sharpe']),4)))
+    
+    st.write('Performance under highest Sharpe Ratio')
+    st.table(maxSharpe_perform)
+    
+    st.write('Weights for each stock')
+    st.bar_chart(maxSharpe_weights, x = 'ticker', y = 'weight')
 
 
 
